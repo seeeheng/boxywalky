68a0a81950b1b875ee13aa79a8c99a277cba4168
user13 <user13@Ubuntu-GPU2.poc.local>
2020-10-16 11:18:05 +0800

    results from vdi

--- ddpg_logger.py
+++ ddpg_logger.py
@@ -22,12 +22,12 @@ class DDPGLogger:
 
 	def check(self, n_episode):
 		current_time = time.time()
-		if current_time - self.start_time > 5:
+		if current_time - self.start_time > 60*30:
 			self.update_time_and_episodes(current_time, n_episode)
 			self.make_csv()
 			self.start_time = current_time
 
 	def make_csv(self):
 		self.n_log += 1
-		pd.DataFrame(data=zip(self.time,self.episodes),columns=("time","episodes")).to_csv("benchmark/log{}.csv".format(self.n_log),index=False)
+		pd.DataFrame(data=zip(self.time,self.episodes),columns=("time","episodes")).to_csv("benchmark/log.csv",index=False)
 		pd.Series(data=self.scores).to_csv("benchmark/scores.csv",index=True)
